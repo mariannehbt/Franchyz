6d3681b32acd0e00a7be6d4b2e740c870b086c55
carolin.labinski <carolin.labinski@yahoo.de>
2020-06-19 16:19:52 +0200

    add team form

--- src/app.jsx
+++ src/app.jsx
@@ -1,33 +1,50 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import 'bootstrap/dist/css/bootstrap.css';
-import 'bootstrap/dist/js/bootstrap.js';
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap/dist/js/bootstrap.js";
 // Redux
-import { Provider } from 'react-redux';
-import store from './redux/store.js';
+import { Provider } from "react-redux";
+import store from "./redux/store.js";
 
 // Pages
-import Navbar from './components/layouts/navbar.jsx'
-import Footer from './components/layouts/footer.jsx'
-import Home from './pages/home.jsx';
-import Login from './pages/login.jsx'
-import Register from './pages/register.jsx'
-import AdminCoachDashboardPage from './pages/adminCoachDashboardPage.jsx'
+import Navbar from "./components/layouts/navbar.jsx";
+import Footer from "./components/layouts/footer.jsx";
+import Home from "./pages/home.jsx";
+import Login from "./pages/login.jsx";
+import Register from "./pages/register.jsx";
+import CreateTeam from "./pages/createTeam.jsx";
+import AdminCoachDashboardPage from "./pages/adminCoachDashboardPage.jsx";
 
 const App = () => {
 	return (
 		<Provider store={store}>
 			<Router>
-          		<>
-                        <Navbar /> 
-                          <Switch> 
-                            <Route path="/login"> <Login /> </Route> 
-                            <Route path="/register"> <Register /> </Route>
-                            <Route path="/dashboardAdmin"> <AdminCoachDashboardPage /> </Route>
-                            <Route exact path="/"> <Home /> </Route>
-                          </Switch>
-				<Footer />
-                        </>
+				<>
+					<Navbar />
+					<Switch>
+						<Route path="/login">
+							{" "}
+							<Login />{" "}
+						</Route>
+						<Route path="/register">
+							{" "}
+							<Register />{" "}
+						</Route>
+						<Route path="/dashboardAdmin">
+							{" "}
+							<AdminCoachDashboardPage />{" "}
+						</Route>
+						<Route path="/create-team">
+							{" "}
+							<CreateTeam />{" "}
+						</Route>
+						<Route exact path="/">
+							{" "}
+							<Home />{" "}
+						</Route>
+					</Switch>
+					<Footer />
+				</>
 			</Router>
 		</Provider>
 	);
