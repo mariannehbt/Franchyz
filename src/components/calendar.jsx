73874230ba34593990435d28235d3efd228dfa55
Dim <dimitri.kiavue@gmail.com>
2020-06-19 13:32:46 +0200

    [UPDATE] Redirect when click on Calendar

--- src/components/calendar.jsx
+++ src/components/calendar.jsx
@@ -1,19 +1,34 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
+import { useHistory } from "react-router-dom";
+
 import FullCalendar from '@fullcalendar/react'
 import dayGridPlugin from "@fullcalendar/daygrid";
 import timeGridPlugin from "@fullcalendar/timegrid";
 import interactionPlugin from "@fullcalendar/interaction";
 
+// import * as EventsAPI from 'services/Events';
+
 import '../styles/calendar.scss' // webpack must be configured to do this
 
 function Calendar() {
   const [events, setEvents] = useState([])
+  let history = useHistory();
 
   const tmp_event = {title: "Event Now", start: new Date()}
 
-  const handleDateClick = arg => {
-    setEvents([...events, {title: "New Event", start: arg.date}])
+  const addEventManually = arg => {
+    setEvents([...events, {title: "Event Now", start: arg.date}])
   }
+
+  const goToEventNew = () => {
+    history.push('/register')
+  }
+
+  const getEvents = () => {
+    // EventsAPI.GetEventsRequest
+  }
+
+  // useEffect(getEvents, [])
 
 
   return (
@@ -39,7 +54,7 @@ function Calendar() {
         week: 'Semaine',
       }}
       events={events}
-      dateClick={handleDateClick}
+      dateClick={goToEventNew}
     />
   )
 }
