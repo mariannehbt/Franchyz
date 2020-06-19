3c48fea379131b9042d24dd4b16595edbe4a25b8
floury-fabio <floury-fabio@hotmail.fr>
2020-06-19 09:09:41 +0200

    dash in prog

--- src/services/clubAPI.jsx
+++ src/services/clubAPI.jsx
@@ -1,11 +1,34 @@
 export const getAllClubs = () => {
-	return fetch('http://localhost:3000/clubs.json', {
-		headers: {
-			'Content-Type': 'application/json'
-		}
-	})
-	.then(response => response.json())
-	.then(response => {
-		return response
-	})
+
+  let baseURL = process.env.REACT_APP_API_URL;
+  let endUrl = 'clubs.json'
+  let url = baseURL + endUrl
+  return fetch(url, {
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  })
+    .then(response => response.json())
+    .then(response => {
+      return response
+    })
 };
+
+function getClub(id) {
+
+  let baseURL = process.env.REACT_APP_API_URL;
+  let endUrl = `clubs${id}.json`
+  let url = baseURL + endUrl
+
+  let headers = {
+    'Content-Type': 'application/json'
+  }
+
+  let request = {
+    headers: headers
+  }
+
+  fetch(url, request)
+    .then()
+
+}
