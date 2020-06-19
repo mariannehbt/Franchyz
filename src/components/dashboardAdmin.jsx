2f438ec1fbe9eb74070c39e4bef18e91f4be68af
floury-fabio <floury-fabio@hotmail.fr>
2020-06-19 18:13:40 +0200

    clean console

--- src/components/dashboardAdmin.jsx
+++ src/components/dashboardAdmin.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import InformationsClub from './InformationsClub.jsx'
 
 function DashboardAdmin() {
--- src/helpers/misc.jsx
+++ src/helpers/misc.jsx
@@ -1,5 +1,3 @@
-import jwt_decode from 'jwt-decode';
-
 function pluralyzeType(type) {
   if(type === 'coach')
     return 'coaches'
