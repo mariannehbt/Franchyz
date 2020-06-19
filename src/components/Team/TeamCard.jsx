02795a9ec42b37dabcbb1a6472974e13f07a53f3
PabloSKZ <pablo.eskenazi@gmail.com>
2020-06-19 18:32:17 +0200

    TeamCard ok

--- src/components/Team/TeamCard.jsx
+++ src/components/Team/TeamCard.jsx
@@ -5,28 +5,56 @@ import { useSelector } from 'react-redux';
 
 
 const TeamCard = () => {
+  const [playersData, setPlayersData] = useState([]);
+  const [teamData, setTeamData] = useState([]);
   const clubID = useSelector(state => state.userReducer.club_id);
   const { teamID } = useParams();
   const isAdmin = useSelector(state => state.userReducer.isAdmin);
   
-  console.log(teamID + "|" + clubID)
-
 	const getData = () => {
 		api.getTeam(clubID, teamID)
 		.then(response => {
       console.log(response)
-			// let teams = response.map((team, key) => (
-			// 	<Link to={`/team/${team.id}`} key={key}><li className='list-group-item'>{team.title}</li></Link>
-			// ));
-			// setData(teams);
+			let players = response.players.map((player, key) => (
+      <tr key={key}>
+        <td>{player.first_name} {player.last_name}</td>
+        <td>{player.jersey_number}</td>
+        <td>{player.position}</td>
+        <td>{player.height}cm</td>
+        <td>{player.weight}kg</td>
+        <td>{player.email}</td>
+        <td>{player.phone}</td>
+      </tr>
+      ));
+      let team = response.title;
+      setPlayersData(players);
+      setTeamData(team);
 		});
 	};
 
   useEffect(getData, []);
 
-  return (<div>
-    ok
-  </div>)
+  return (
+  <div>
+    <h1>{teamData}</h1>
+    <table class="table">
+      <thead>
+        <tr>
+          <th scope="col">Name</th>
+          <th scope="col">Jersey</th>
+          <th scope="col">Position</th>
+          <th scope="col">Height</th>
+          <th scope="col">Weight</th>
+          <th scope="col">Email</th>
+          <th scope="col">Phone</th>
+        </tr>
+      </thead>
+      <tbody>
+        {playersData}
+      </tbody>
+    </table>
+  </div>
+  )
 };
 
 export default TeamCard;
