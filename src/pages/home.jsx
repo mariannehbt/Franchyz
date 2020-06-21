206d7fcbcbf8895644f7feb95b4907385e56ba67
Dim <dimitri.kiavue@gmail.com>
2020-06-21 15:19:24 +0200

    [ADD] Calendar on Dashboard Player

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,15 +1,10 @@
 import React from 'react';
 import Carousel from '../components/carousel';
-import Calendar from '../components/calendar'
-import PlayerList from '../components/Player/playerList'
 
 function Home() {
-
-
   return(
       <>
         <Carousel />
-        <Calendar />
       </>
   )
 }
