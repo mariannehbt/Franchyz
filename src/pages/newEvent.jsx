92f26ef6210e2eb1c3e438aef3e66913392ddec2
floury-fabio <floury-fabio@hotmail.fr>
2020-06-21 15:26:43 +0200

    setup new event for request

--- src/pages/newEvent.jsx
+++ src/pages/newEvent.jsx
@@ -1,11 +1,26 @@
+import React, { useState, useEffect } from "react";
 import FormGame from "components/formGame.jsx";
-import React, { useState } from "react";
 import { Select } from "antd";
 import FormPractice from "components/formPractice";
+import PlayerList from "components/playerList";
+import { useSelector, useDispatch } from 'react-redux';
+import * as teamAPI from 'services/teamAPI'
 
 function CreateEvents() {
+
+  const clubId = useSelector(state => state.userReducer.club_id);
   const [eventType, setEventType] = useState("");
+  const [teams, setTeams] = useState('')
   const { Option } = Select;
+
+  useEffect(() => { setupTeams() }, [])
+
+  async function setupTeams(){
+    const ans = await teamAPI.getTeamsOfClub(clubId)
+    console.log(ans)
+    //setTeams(ans)
+        //<PlayerList players={teams[0].players} />
+  }
 
   function onChange(value) {
     setEventType(value);
@@ -17,6 +32,7 @@ function CreateEvents() {
       <br />
       <hr className="my-4" style={{ width: "600px" }}></hr>
       <div className="bg-dark pb-3 p-2 mx-auto rounded select" style={{ width: "35%" }} >
+
         <h3 className="text-light text-center">Choisir le type d'événement?</h3>
         <div className="text-center">
           <Select className="text-center" showSearch style={{ width: 300 }} placeholder="Choisir le type d'evenement" optionFilterProp="children" onChange={onChange} filterOption={(input, option) => option.children.toLowerCase().indexOf(input.toLowerCase()) >= 0 } >
