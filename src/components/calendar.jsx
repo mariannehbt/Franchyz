ae267ebe426008b3ac3e67a8c3901b222cc7a1e5
Dim <dimitri.kiavue@gmail.com>
2020-06-21 12:42:00 +0200

    [UPDATE] Component Calendar

--- src/components/calendar.jsx
+++ src/components/calendar.jsx
@@ -13,7 +13,8 @@ import * as EventsAPI from 'services/eventsAPI';
 import '../styles/calendar.scss' // webpack must be configured to do this
 
 function Calendar() {
-  const [events, setEvents] = useState([])
+  const [games, setGames] = useState([])
+  const [practices, setPractices] = useState([])
   const user_id = 2
   const club_id = 1
   const team_id = 1
@@ -22,21 +23,35 @@ function Calendar() {
   const tmp_event = {title: "Event Now", start: new Date()}
 
   const addEventManually = arg => {
-    setEvents([...events, {title: "Event Now", start: arg.date}])
+    setGames([...games, {title: "Event Now", start: arg.date}])
   }
 
   const goToEventNew = () => {
     history.push('/register')
   }
 
-  const getEvents =() => {
+  const getAllEvents = () => {
+    getGames()
+    getPractices()
+  }
+
+  const getGames =() => {
     EventsAPI.getAttendedGames(user_id, club_id, team_id)
     .then(response => {
-      response.map(game => setEvents([...events, {title: game.title, start: game.starting_date_time}]));
+      response.map(game => setGames([...games, {title: game.title, start: game.starting_date_time}]));
     })
   }
 
-  useEffect(() => { getEvents () }, [])
+  const getPractices =() => {
+    EventsAPI.getAttendedGames(user_id, club_id, team_id)
+    .then(response => {
+      response.map(practice => setGames([...games, {title: practice.title, start: practice.starting_date_time}]));
+    })
+  }
+
+  useEffect(() => {
+    getAllEvents()
+  }, [])
 
 
   return (
@@ -49,19 +64,20 @@ function Calendar() {
               center: "title",
               right: "dayGridMonth,timeGridWeek,timeGridDay,listWeek"
             }}
-      allDaySlot="false"
+      allDaySlot={false}
       firstDay="1"
       locale= "fr"
       timeZone="UTC"
-      minTime='09:00:00'
-      maxTime='23:00:00'
       buttonText= {{
         today:"Aujourd'hui",
         day: 'Jour',
         month: 'Mois',
         week: 'Semaine',
       }}
-      events={events}
+      events={{
+        games,
+        practices
+      }}
       dateClick={goToEventNew}
     />
   )
--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -9,7 +9,7 @@ function Home() {
   return(
       <>
         <Carousel />
-        <PlayerList />
+        <Calendar />
       </>
   )
 }
