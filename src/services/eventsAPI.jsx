2bb5314e7f016831763e2ca42c825d48c4193180
carolin.labinski <carolin.labinski@yahoo.de>
2020-06-23 00:53:38 +0200

    add removed fetch for createGame

--- src/services/eventsAPI.jsx
+++ src/services/eventsAPI.jsx
@@ -1,5 +1,47 @@
 import Cookies from 'js-cookie'
 
+
+function createGame(
+	EventTitle,
+	EventDescription,
+	Address,
+	City,
+	Country,
+	ZipCode,
+	DateTime,
+	Duration,
+	Club_id,
+	Team_id
+) {
+	const data = {
+		title: EventTitle,
+		long_description: EventDescription,
+		address: Address,
+		city: City,
+		country: Country,
+		zip_code: ZipCode,
+		starting_date_time: DateTime,
+		duration: Duration,
+		canceled: false,
+	};
+
+	let club_id = Club_id;
+	let team_id = Team_id;
+	let baseURL = process.env.REACT_APP_API_URL;
+	let endUrl = `/clubs/${club_id}/teams/${team_id}/games.json`;
+	let url = baseURL + endUrl;
+
+	return fetch(url, {
+		method: "post",
+		headers: {
+			"Content-Type": "application/json",
+		},
+		body: JSON.stringify(data),
+	}).then((response) => {
+		return response;
+	});
+}
+
 function createPractice( EventTitleP, EventDescriptionP, AddressP, CityP, CountryP, ZipCodeP, DateTimeP, DurationP, Club_id, Team_id) {
 
 	const data = {
