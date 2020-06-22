9a042289522cded55660c227f2558caccc791ac5
carolin.labinski <carolin.labinski@yahoo.de>
2020-06-23 00:33:41 +0200

    add bootstrap card

--- src/pages/showPractice.jsx
+++ src/pages/showPractice.jsx
@@ -4,8 +4,6 @@ import * as eventsAPI from 'services/eventsAPI.jsx'
 import moment from 'moment'
 import "../styles/app.scss";
 import localization from 'moment/locale/fr';
-import { Card } from 'react-bootstrap';
-
 
 function PracticeShow() {
 	let {practice_id} = useParams();
@@ -16,34 +14,29 @@ function PracticeShow() {
 	}, [])
 
 	  console.log("response" + practice.canceled);
+	  console.log("response" + practice.players);
 	  moment.updateLocale('fr', localization);
 
 	return <>
-
-<Card style={{height:"350px", width:"400px", marginLeft:"100px", marginTop:"100px"}}>
-  <Card.Header as="h5">	<div className="text-uppercase">{practice.title}</div> </Card.Header>
-  <Card.Body>
-    <Card.Title>{practice.description}</Card.Title>
-    <Card.Text>
+<div className="card" style={{height:"350px", width:"400px", marginLeft:"100px", marginTop:"100px"}}>
+  <div className="card-header">
+  <div className="text-uppercase">{practice.title}</div> 
+  </div>
+  <div className="card-body">
 	<h6>Location</h6>
 	<p> {practice.address}</p>
 	<p> {practice.city}</p>
 	<p> {practice.zip_code} {practice.country}</p>
-
 	<br/>
 	<h6> Date and Time </h6>
 	<p> {moment(practice.starting_date_time).format("LLLL")}</p>
 	<h6> Duration in min: </h6>
 	<p>{practice.duration}</p>
 	{ practice.canceled !== "false" ? "" : <h6 className="redtext">The event is canceled.</h6>
-
 	}
-    </Card.Text>
-  </Card.Body>
-</Card>
+  </div>
+</div>
 	
-
-
 	</>;
 }
 
