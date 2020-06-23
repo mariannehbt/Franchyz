e652605539ba8e29a5920224f9f9b5f935473064
carolin.labinski <carolin.labinski@yahoo.de>
2020-06-23 14:58:17 +0200

    game

--- src/components/formGame.jsx
+++ src/components/formGame.jsx
@@ -47,11 +47,11 @@ const FormGame = ({ playersIds }) => {
   async function onSubmit() {
     if (dateTime === "") {
       document.getElementById("notice_datetime").innerHTML =
-        "Merci de choisir une date";
+        "Please fill in a start date";
     }
     if (eventTitle === "") {
       document.getElementById("notice_title").innerHTML =
-        "Merci de saisir un titre";
+        "Please fill in a title";
     }
 
     let game = await gameAPI.createGame(club_id, team_id, eventTitle, eventDescription, address, city, country, zipCode, dateTime, duration)   
@@ -65,15 +65,15 @@ const FormGame = ({ playersIds }) => {
         <Row>
           <Col span={10} offset={8}>
             <h3>Competition:</h3>
-            <label>Date et heure de la competition:</label>
+            <label>Date and time of competition:</label>
             <br />
             <DatePicker id="datetime" format="DD-MM-YY HH:mm" disabledDate={disabledDate} onChange={onChange} onOk={onOk} showTime={{ defaultValue: moment("00:00:00", "HH:mm:ss") }} />
             <p id="notice_datetime" className="redtext"></p>
 
             {dateTime !== "" && (
-              <h6 style={{ marginTop: "25px" }}> Date choisie: {dateTime}</h6>
+              <h6 style={{ marginTop: "25px" }}> Selected date and time: {dateTime}</h6>
             )}
-            <label>Durée de la competition en min:</label>
+            <label>Duration in min:</label>
             <br />
             <InputNumber style={{ marginBottom: "15px" }} defaultValue={0} step={5} min={0} max={100000} formatter={(valueMin) => `${valueMin}`} parser={(valueMin) => valueMin.replace(" min", "")} onChange={onChangeDuration} />
           </Col>
@@ -82,34 +82,34 @@ const FormGame = ({ playersIds }) => {
         <Row>
           <Col span={8} offset={8}>
             <div className="form-group row col-12">
-              <label style={{ marginLeft: "10px", color: "grey" }}>Titre:</label>
-              <input type="text" className="form-control" placeholder="Saisir un titre" id="title" onChange={(e) => setEventTitle(e.target.value)} value={eventTitle} />
+              <label style={{ marginLeft: "10px", color: "grey" }}>Title:</label>
+              <input type="text" className="form-control" placeholder="Title" id="title" onChange={(e) => setEventTitle(e.target.value)} value={eventTitle} />
               <p id="notice_title" className="redtext"></p>
             </div>
 
             <div className="form-group row col-12">
-              <label style={{ marginLeft: "10px", color: "grey" }}>Déscription:</label>
-              <input type="text" className="form-control" placeholder="Saisir une description" id="description" onChange={(e) => setEventDescription(e.target.value)} value={eventDescription} />
+              <label style={{ marginLeft: "10px", color: "grey" }}>Description:</label>
+              <input type="text" className="form-control" placeholder="Description" id="description" onChange={(e) => setEventDescription(e.target.value)} value={eventDescription} />
             </div>
             <h3>L'adresse de la competition:</h3>
             <div className="form-group row col-12 ">
-              <label style={{ marginLeft: "10px", color: "grey" }}>Adresse:</label>
-              <input type="text" className="form-control" placeholder="L'adresse" id="address" onChange={(e) => setAddress(e.target.value)} value={address} />
+              <label style={{ marginLeft: "10px", color: "grey" }}>Address:</label>
+              <input type="text" className="form-control" placeholder="Address" id="address" onChange={(e) => setAddress(e.target.value)} value={address} />
             </div>
 
             <div className="form-group row col-12">
-              <label style={{ marginLeft: "10px", color: "grey" }}>Code Postal:</label>
-              <input type="text" className="form-control" placeholder="Code postal" id="zipcode" onChange={(e) => setZipCode(e.target.value)} value={zipCode} />
+              <label style={{ marginLeft: "10px", color: "grey" }}>Zip code:</label>
+              <input type="text" className="form-control" placeholder="Zip code" id="zipcode" onChange={(e) => setZipCode(e.target.value)} value={zipCode} />
             </div>
 
             <div className="form-group row col-12">
-              <label style={{ marginLeft: "10px", color: "grey" }}>Ville:</label>
-              <input type="text" className="form-control" placeholder="Ville" id="city" onChange={(e) => setCity(e.target.value)} value={city} />
+              <label style={{ marginLeft: "10px", color: "grey" }}>City:</label>
+              <input type="text" className="form-control" placeholder="City" id="city" onChange={(e) => setCity(e.target.value)} value={city} />
             </div>
 
             <div className="form-group row col-12">
-              <label style={{ marginLeft: "10px", color: "grey" }}>Pays:</label>
-              <input type="text" className="form-control" placeholder="Pays" id="country" onChange={(e) => setCountry(e.target.value)} value={country} />
+              <label style={{ marginLeft: "10px", color: "grey" }}>Country:</label>
+              <input type="text" className="form-control" placeholder="Country" id="country" onChange={(e) => setCountry(e.target.value)} value={country} />
             </div>
           </Col>
         </Row>
@@ -117,7 +117,7 @@ const FormGame = ({ playersIds }) => {
         <Row>
           <Col span={5} offset={11}>
             <button type="submit" className="btn btn-outline-danger" style={{ marginTop: "25px", marginBottom: "25px" }} onClick={onSubmit} >
-              Sauvegarder
+              Save
             </button>
           </Col>
         </Row>
