0a08dd57bb618ec0ac775314d14358b43af39bb0
carolin.labinski <carolin.labinski@yahoo.de>
2020-06-23 18:10:15 +0200

    all routes done

--- src/components/privateRoute.jsx
+++ src/components/privateRoute.jsx
@@ -1,17 +1,18 @@
 import React from 'react'
 import { Route, useHistory } from "react-router-dom";
 import Cookies from "js-cookie";
+import {useSelector} from "react-redux";
 
 const PrivateRoute = ({ component: Component, ...rest }) => {
+    const current_user = useSelector((state) => state.userReducer.id);
+console.log("current user" + current_user)
 	const history = useHistory();
-
-
-		// useEffect blabla fetch fetch
 
 		return (
 			<Route{...rest} render={(props) =>
 					
-					Cookies.get('token') ? 
+                     Cookies.get('token') ? 
+                   
 
 					(<Component {...props} />) 
 
