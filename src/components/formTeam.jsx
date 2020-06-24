e10e7b423d89fab11f894eb91b4c6ba97aebbea2
floury-fabio <floury-fabio@hotmail.fr>
2020-06-24 09:23:55 +0200

    clean code

--- src/components/formTeam.jsx
+++ src/components/formTeam.jsx
@@ -1,58 +1,54 @@
 import React, { useState } from "react";
 import "../styles/app.scss";
-import * as API from "services/teamAPI";
+import * as teamAPI from "services/teamAPI";
 import { useSelector } from "react-redux";
+import { useHistory } from "react-router-dom";
+
 
 const FormTeam = () => {
-	const Creator_id = useSelector((state) => state.userReducer.id);
-	const Club_id = useSelector((state) => state.userReducer.club_id);
-	const Coach_id = useSelector((state) => state.userReducer.id);
-	const [TeamName, setTeamName] = useState("");
+  const creatorId = useSelector((state) => state.userReducer.id);
+  const clubId = useSelector((state) => state.userReducer.club_id);
+  const coachId = useSelector((state) => state.userReducer.id);
 
-	function onSubmit() {
-		if (TeamName === "") {
-			document.getElementById("notice_teamname").innerHTML =
-				"Please fill in a team name";
-		}
-		console.log(TeamName);
+  const [teamName, setTeamName] = useState("");
 
-		API.createTeam(TeamName, Creator_id, Coach_id, Club_id).then((response) => {
-			console.log(response);
-		});
-	}
+  let history = useHistory();
 
-	return (
-		<div>
-			<div className="text-center">
-				<div className="container">
-					<div className="row d-flex align-items-center">
-						<div className=" form-group col-4 text-center pb-3 p-2 mx-auto">
-							<label style={{ marginLeft: "10px", color: "grey" }}>
-								Team name:
-							</label>
-							<input
-								type="text"
-								className="form-control"
-								placeholder="Name"
-								id="title"
-								onChange={(e) => setTeamName(e.target.value)}
-								value={TeamName}
-							/>
-							<p id="notice_teamname" className="redtext"></p>
-						</div>
-					</div>
-				</div>
-				<button
-					type="submit"
-					className="btn btn-outline-danger"
-					style={{ marginTop: "25px", marginBottom: "25px" }}
-					onClick={onSubmit}
-				>
-					Save
-				</button>
-			</div>
-		</div>
-	);
+  const onSubmit = async () => {
+    if (teamName === "") {
+      document.getElementById("notice_teamname").innerHTML =
+        "Please fill in a team name";
+    }
+
+    let response = await teamAPI.createTeam(teamName, creatorId, coachId, clubId)
+
+    if (response.errors === undefined) {
+      history.push("/dashboardAdmin");
+    }
+
+  }
+
+  return (
+    <div>
+      {alert}
+      <div className="text-center">
+        <div className="container">
+          <div className="row d-flex align-items-center">
+            <div className=" form-group col-4 text-center pb-3 p-2 mx-auto">
+              <label style={{ marginLeft: "10px", color: "grey" }}>
+                Team name:
+              </label>
+              <input type="text" className="form-control" placeholder="Name" id="title" onChange={(e) => setTeamName(e.target.value)} value={teamName} />
+              <p id="notice_teamname" className="redtext"></p>
+            </div>
+          </div>
+        </div>
+        <button type="submit" className="btn btn-outline-danger" style={{ marginTop: "25px", marginBottom: "25px" }} onClick={onSubmit} >
+          Save
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default FormTeam;
--- src/components/teamList.jsx
+++ src/components/teamList.jsx
@@ -1,19 +1,9 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
-import { useSelector } from 'react-redux';
 import Team from 'components/team.jsx'
 
 const TeamList = ({teams}) => {
-  const isAdmin = useSelector(state => state.userReducer.isAdmin)
 
-
-  const createTeam = () => {
-    if (isAdmin) {
-      return <p><Link to={`/create-team`} className="btn btn-primary mt-3">Create New Team</Link></p>
-    } 
-  }
-
-  function setList() {
+  const setList = () => {
     if (teams !== undefined){
       return teams.map(team => <Team key={team.id} team={team} />)
     }
@@ -23,7 +13,6 @@ const TeamList = ({teams}) => {
     return (
       <div className="container">
         <p>This club don't have team yet !</p>
-        {createTeam()}
       </div>
     );
   } else {
@@ -32,7 +21,6 @@ const TeamList = ({teams}) => {
         <ul className='list-group list-group-flush'>
           {setList()}
         </ul>
-        {createTeam()}
       </div>
     );
   }
