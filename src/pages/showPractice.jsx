cd3fe4fde45b10a9a6fcde527a68d7456464ac4f
carolin.labinski <carolin.labinski@yahoo.de>
2020-06-25 11:56:21 +0200

    rm create game, create practice, move getPrctice, indentation, rm br, change style

--- src/pages/showPractice.jsx
+++ src/pages/showPractice.jsx
@@ -1,45 +1,45 @@
 import React, {useState, useEffect} from "react";
 import {useParams} from "react-router-dom";
-import * as eventsAPI from 'services/eventsAPI.jsx'
+import * as practiceAPI from 'services/practiceAPI.jsx'
 import moment from 'moment'
 import "../styles/app.scss";
+import "../styles/showPractice.scss";
 import localization from 'moment/locale/fr';
 
 function PracticeShow() {
 	let {practice_id} = useParams();
-	 const [practice, setPractice] = useState("");
+	const [practice, setPractice] = useState("");
 
-	 useEffect(() => {  eventsAPI.getPractice(practice_id).then((response) => setPractice(response));
-	
-	},[] )
+	useEffect(() => {  
+		practiceAPI.getPractice(practice_id)
+		.then((response) => setPractice(response));
+		},[] )
 
-	  console.log("response" + practice.canceled);
-	  console.log("response" + practice.players);
-	  moment.updateLocale('fr', localization);
-
-	  console.log("status" + practice.status);
+	moment.updateLocale('fr', localization);
 
 	return <>
-	{ practice.status === 404 ? <h3 className="text-center redtext mt-5">Ths practice does not exist.</h3> : <div className="card" style={{height:"350px", width:"400px", marginLeft:"100px", marginTop:"100px"}}>
-  <div className="card-header">
-  <div className="text-uppercase">{practice.title}</div> 
-  </div>
-  <div className="card-body">
-	<h6>Location</h6>
-	<p> {practice.address}</p>
-	<p> {practice.city}</p>
-	<p> {practice.zip_code} {practice.country}</p>
-	<br/>
-	<h6> Date and Time </h6>
-	<p> {moment(practice.starting_date_time).format("LLLL")}</p>
-	<h6> Duration in min: </h6>
-	<p>{practice.duration}</p>
-	{ practice.canceled !== "false" ? "" : <h6 className="redtext">The event is canceled.</h6>
-	}
-  </div>
-</div>}
-
-	
+	{practice.status === 404 ? 
+	<h3 className="text-center redtext mt-5">Ths practice does not exist.</h3> : 
+	<div className="card mx-auto">
+			<div className="card-header">
+				<div className="text-uppercase">
+					{practice.title}
+				</div> 
+			</div>
+						<div className="card-body">
+							<h6>Location</h6>
+								<p> {practice.address}</p>
+								<p> {practice.city}</p>
+								<p> {practice.zip_code} {practice.country}</p>
+								<h6> Date and Time </h6>
+								<p> {moment(practice.starting_date_time).format("LLLL")}</p>
+								<h6> Duration in min: </h6>
+								<p>{practice.duration}</p>
+								{practice.canceled !== "false" ? 
+								"" : 
+								<h6 className="redtext">The event is canceled.</h6>}
+						</div>
+		</div>}
 	</>;
 }
 
--- src/services/eventsAPI.jsx
+++ src/services/eventsAPI.jsx
@@ -1,96 +1,5 @@
 import Cookies from 'js-cookie'
-
-
-function createGame(
-	EventTitle,
-	EventDescription,
-	Address,
-	City,
-	Country,
-	ZipCode,
-	DateTime,
-	Duration,
-	Club_id,
-	Team_id
-) {
-	const data = {
-		title: EventTitle,
-		long_description: EventDescription,
-		address: Address,
-		city: City,
-		country: Country,
-		zip_code: ZipCode,
-		starting_date_time: DateTime,
-		duration: Duration,
-		canceled: false,
-	};
-
-	let club_id = Club_id;
-	let team_id = Team_id;
-	let baseURL = process.env.REACT_APP_API_URL;
-	let endUrl = `/clubs/${club_id}/teams/${team_id}/games.json`;
-	let url = baseURL + endUrl;
-
-	return fetch(url, {
-		method: "post",
-		headers: {
-			"Content-Type": "application/json",
-		},
-		body: JSON.stringify(data),
-	}).then((response) => {
-		return response;
-	});
-}
-
-function createPractice( EventTitleP, EventDescriptionP, AddressP, CityP, CountryP, ZipCodeP, DateTimeP, DurationP, Club_id, Team_id) {
-
-	const data = {
-		title: EventTitleP,
-		long_description: EventDescriptionP,
-		address: AddressP,
-		city: CityP,
-		country: CountryP,
-		zip_code: ZipCodeP,
-		starting_date_time: DateTimeP,
-		duration: DurationP,
-		canceled: false,
-	};
-
-	let club_id = Club_id;
-	let team_id = Team_id;
-	let baseURL = process.env.REACT_APP_API_URL;
-	let endUrl = `/clubs/${club_id}/teams/${team_id}/practices.json`;
-	let url = baseURL + endUrl;
-
-	return fetch(url, {
-		method: "post",
-		headers: {
-			"Content-Type": "application/json",
-		},
-		body: JSON.stringify(data),
-	}).then((response) => {
-		return response;
-	});
-}
-
-function getPractice(practice_id) {
-		let baseURL = process.env.REACT_APP_API_URL;
-		let endUrl = `/practices/${practice_id}.json`
-		let url = baseURL + endUrl
-	  
-		let headers = {
-		  'Content-Type': 'application/json'
-		}
-	  
-		let request = {
-		  headers: headers
-		}
-		
-		return fetch(url, request)
-		  .then(response => response.json())
-		  .then(response => { return response })
-	  
-	  }
+ 
 const getUnconfirmedEvents = (player_id, club_id, team_id) => {
 
   let headers = {
@@ -239,5 +148,5 @@ const confirmAttendance = (player_id, club_id, team_id, event_id) => {
     })
 }
 
-export { createPractice, getUnconfirmedEvents, getAttendedGames, getUnattendedGames, getAttendedPractices, getUnattendedPractices, confirmAttendance, createGame, getPractice };
+export { getUnconfirmedEvents, getAttendedGames, getUnattendedGames, getAttendedPractices, getUnattendedPractices, confirmAttendance };
 
