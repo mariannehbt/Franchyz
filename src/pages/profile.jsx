405325e19f46cb8b05bd12cf13b91bfdd075c38e
floury-fabio <floury-fabio@hotmail.fr>
2020-06-26 07:39:32 +0200

    setup profile page

--- src/pages/profile.jsx
+++ src/pages/profile.jsx
@@ -1,10 +1,26 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import ProfileEdit from 'components/profileEdit';
+import * as playerAPI from 'services/playerAPI'
+import { useParams } from 'react-router-dom'
+
+const [player, setPlayer] = useState('')
+
+let { clubId, teamId, playerId } = useParams();
+
+
+  useEffect(() => {
+    loadPlayer();
+  }, []);
+
+  const loadPlayer = async () => {
+    const response = await playerAPI.getPlayer(clubId, teamId, playerId);
+    setPlayer(response);
+  }
 
 const Profile = () => {
   return (
     <div className='text-center mt-5'>
-      <ProfileEdit />
+      <ProfileEdit player={player} />
     </div>
   );
 };
