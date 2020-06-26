fe41937c1f4cdea37b19f41c1082045e2858e47b
floury-fabio <floury-fabio@hotmail.fr>
2020-06-26 07:38:38 +0200

    rework player

--- src/components/player.jsx
+++ src/components/player.jsx
@@ -1,33 +1,14 @@
-import React, { useEffect, useRef } from 'react';
-import { Link, useLocation } from 'react-router-dom';
+import React, { useRef } from 'react';
+import { Link } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 
 const Player = ({player, handleCheckboxChange, checkbox}) => {
-  const location = useLocation()
   const clubId = useSelector(state => state.userReducer.clubId);
   
-  function test(e) {
-    console.log(checkbox)
-    handleCheckboxChange(e, checkbox)
-  }
 
-  function setupElements() {
+  const setupElements = () => {
     if (player !== undefined){ 
-      if (location.pathname === '/newEvent')
-      {
-        return (
-          <li className='list-group-item'> 
-            <Link to={`/clubs/${clubId}/teams/${player.team_id}/players/${player.id}`}> {player.first_name} </Link> 
-            <input type="checkbox" className="form-check-input" id={`checkbox ${player.id}`} name={`checkbox${player.id}`} onChange={test} />
-            <Link to={`/clubs/${clubId}/teams/${player.team_id}/players/${player.id}`}> {player.first_name} </Link> 
-            <input type="checkbox" className="form-check-input" id={`checkbox ${player.id}`} name='dqdq' onChange={test} />
-          </li>
-        ) 
-      }
-      else
-      {
-        return <li className='list-group-item'> <Link to={`/clubs/${clubId}/teams/${player.team_id}/players/${player.id}`}> {player.first_name} </Link>  </li>
-      }
+      return <li className='list-group-item'> <Link to={`/clubs/${clubId}/teams/${player.team_id}/players/${player.id}`}> {player.first_name} </Link>  </li>
     }
   }
   return (
