20d94370af7d6f990ea3625acd26bedf2bf835f5
floury-fabio <floury-fabio@hotmail.fr>
2020-06-26 10:55:09 +0200

    rework userApi

--- src/pages/profile.jsx
+++ src/pages/profile.jsx
@@ -1,30 +1,31 @@
 import React, { useEffect, useState } from 'react';
 import ProfileEdit from 'components/profileEdit';
 import ProfileShow from 'components/profileShow';
-import * as userAPI from 'services/playerAPI'
+import * as userAPI from 'services/userAPI'
 import { useParams } from 'react-router-dom'
 import { useSelector } from 'react-redux'
 
-const [profile, setProfile] = useState('')
-const userId = useSelector(state => state.userReducer.userId);
-
-let { clubId, teamId, playerId } = useParams();
-
-
-useEffect(() => {
-  setupElements()
-}, []);
-
-const setupElements = async () => {
-  const response = await userAPI.getPlayer(clubId, teamId, playerId);
-  if (userId === playerId) {
-    setProfile(<ProfileEdit player={response} />);
-  } else {
-    setProfile(<ProfileShow player={response} />);
-  }
-}
 
 const Profile = () => {
+
+  const [profile, setProfile] = useState('')
+  const userId = useSelector(state => state.userReducer.userId);
+
+  let { clubId, teamId, playerId } = useParams();
+
+
+  useEffect(() => {
+    setupElements()
+  }, []);
+
+  const setupElements = async () => {
+    const response = await userAPI.getPlayer(clubId, teamId, playerId);
+    if (userId === playerId) {
+      setProfile(<ProfileEdit player={response} />);
+    } else {
+      setProfile(<ProfileShow player={response} />);
+    }
+  }
   return (
     <div className='text-center mt-5'>
       {profile}
--- src/services/userAPI.jsx
+++ src/services/userAPI.jsx
@@ -1,28 +1,6 @@
 import Cookies from 'js-cookie';
 import { pluralyzeType } from 'helpers/misc.jsx';
 
-const profile = (id, type) => {
-  type = pluralyzeType(type);
-
-  let baseURL = process.env.REACT_APP_API_URL;
-  let endUrl = `/${type}/${id}.json`;
-  let url = baseURL + endUrl;
-
-  let headers = {
-    'Content-Type': 'application/json',
-    Authorization: Cookies.get('token')
-  };
-
-  let request = {
-    method: 'get',
-    headers: headers,
-  };
-
-  return fetch(url, request)
-    .then(response => response.json())
-    .then(response => {return response})
-};
-
 const getPlayer = (clubId, teamId, playerId) => {
 
   let baseURL = process.env.REACT_APP_API_URL;
@@ -45,45 +23,15 @@ const getPlayer = (clubId, teamId, playerId) => {
 
 }
 
-const profileUpdate = ({ id, type, fields }) => {
-  let data;
-  if (type === 'coach') {
-    data = {
-      first_name: fields.data.first_name,
-      last_name: fields.data.last_name,
-      phone: fields.data.phone,
-      birthdate: fields.data.birthdate,
-      arrival: fields.data.arrival,
-      'admin?': fields.data['admin?'],
-      club_id: fields.data.club_id,
-    };
-  } else if (type === 'player') {
-    data = {
-      first_name: fields.data.first_name,
-      last_name: fields.data.last_name,
-      phone: fields.data.phone,
-      birthdate: fields.data.birthdate,
-      arrival: fields.data.arrival,
-      'availability?': fields.data['availability?'],
-      height: fields.data.height,
-      weight: fields.data.weight,
-      gender: fields.data.gender,
-      jersey_number: fields.data.jersey_number,
-      position: fields.data.position,
-      team_id: fields.data.team_id,
-    };
-  };
-
-  type = pluralyzeType(type);
+const playerUpdate = ({clubId, teamId, playerId, data}) => {
 
   let baseURL = process.env.REACT_APP_API_URL;
-  let endUrl = `/${type}/${id}.json`;
+  let endUrl = `clubs/${clubId}/teams/${teamId}/players/${playerId}`;
   let url = baseURL + endUrl;
 
   let headers = {
     'Content-Type': 'application/json',
     Authorization: Cookies.get('token'),
-    // 'Access-Control-Allow-Methods': '*'
   };
 
   let request = {
@@ -97,4 +45,4 @@ const profileUpdate = ({ id, type, fields }) => {
     .then(response => {return response})
 };
 
-export { profile, profileUpdate, getProfile };
+export { getPlayer, playerUpdate };
